53fff2999833515ada931943f5c1bc0e2edf3af4
Gregor <gregor.fiddes@gmail.com>
2023-06-29 21:25:05 +0100

    deleted
    deleted

--- script.js
+++
@@ -1,9 +0,0 @@
-let topButton = document.getElementById("backToTop");
-
-window.onscroll = () => {
-  topButton.style.display = document.documentElement.scrollTop > 20 ? "block" : "none";
-}
-
-topButton.onclick = function() {
-  window.scrollTo({top: 0, behavior: 'smooth'}); // Smooth scrolling
-}
